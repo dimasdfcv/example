a1a23f5d213789b93c953be8461229318085a64e
dimasdfcv <tkachev_dima@list.ru>
2023-03-25 09:56:40 +0300

    add max_2_chisla

---
+++ max_2_chisla/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число a");
+string input1 = Console.ReadLine();
+int number1 = int.Parse(input1);
+Console.WriteLine("Введите число b");
+string input2 = Console.ReadLine();
+int number2 = int.Parse(input2);
+
+if (number1>number2)
+{
+    Console.WriteLine($"Максимальное число {number1}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число {number2}");
+}
