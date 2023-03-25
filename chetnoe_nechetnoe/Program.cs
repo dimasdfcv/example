21f368eb2e344e2f38b925a9ab10d641bb33b2a2
dimasdfcv <tkachev_dima@list.ru>
2023-03-25 10:06:32 +0300

    add chetnoe_nechetnoe

---
+++ chetnoe_nechetnoe/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число : ");
+string input = Console.ReadLine();
+int number = int.Parse(input);
+  if (number % 2 == 0) Console.WriteLine("Четное");
+  else Console.WriteLine("Нечетное");
