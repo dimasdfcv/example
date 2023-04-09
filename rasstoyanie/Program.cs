881c66053a30c9231ecb22fae9010a1cdbf3c915
dimasdfcv <tkachev_dima@list.ru>
2023-04-09 16:45:29 +0300

    add rasstoyanie

---
+++ rasstoyanie/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+Console.WriteLine("Введите координату X первой точки: ");
+int x1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату Y первой точки: ");
+int y1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату Z первой точки: ");
+int z1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату X второй точки: ");
+int x2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату Y второй точки: ");
+int y2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату Z второй точки: ");
+int z2 = int.Parse(Console.ReadLine());
+
+
+int A = x2 - x1;
+int B = y2 - y1;
+int C = z1 - z2;
+
+double answer = Math.Sqrt(A * A + B * B + C * C);
+Console.WriteLine(Math.Round(answer, 2));
+
+
+
+
