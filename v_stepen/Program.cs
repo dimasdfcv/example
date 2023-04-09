b65832e0b80433d81708eb6164e0db109dd0950c
dimasdfcv <tkachev_dima@list.ru>
2023-04-09 17:15:08 +0300

    add v_stepen

---
+++ v_stepen/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+int stepen = a;
+
+for (int i = 1; i < b; i++)
+{
+stepen = stepen * a;
+}
+Console.WriteLine("A в степени B равно: " + stepen);
