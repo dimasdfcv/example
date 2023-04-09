69b903fe6c6d85b002cff448155e05d2869cb217
dimasdfcv <tkachev_dima@list.ru>
2023-04-09 16:53:49 +0300

    add kub

---
+++ kub/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+for (int i = 1; i < num; i++)
+{
+    int result = i * i * i;
+    Console.Write($"{result}, ");
+}
+Console.Write($"{num*num*num}");
