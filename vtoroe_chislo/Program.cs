80765ed3b9d513ecea28a037a4c6238565a64003
dimasdfcv <tkachev_dima@list.ru>
2023-04-09 15:16:37 +0300

    add vtoroe_chilo

--- vtoroe_chislo/Program.cs
+++ vtoroe_chislo/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+Console.Write("Введите трехзанчное число:");
+int Value = Convert.ToInt32(Console.ReadLine());
+int Length = Value.ToString().Length;
+if (Length == 3) {
+	int digit = ( Value/10 ) % 10;
+	Console.WriteLine("Второе число: " + digit);
+} else {
+	Console.WriteLine("Вы ввели не трехначное число");
+}
