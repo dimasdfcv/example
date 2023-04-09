06c18bc74df301ce91645bf3a28e14be966266cf
dimasdfcv <tkachev_dima@list.ru>
2023-04-09 15:38:58 +0300

    add tretya_tsyfra

---
+++ tretya_tsyfra/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.Write("Enter a number:");
+int Value = Convert.ToInt32(Console.ReadLine());
+int Length = Value.ToString().Length;
+if (Length >= 3) {
+	while (Value > 999)
+	{
+		Value = Value / 10;
+	}
+	int digit = Value % 10;
+	Console.WriteLine("третья цифра " + digit);
+} else {
+	Console.WriteLine("третьей цифры нет");
+}
+
